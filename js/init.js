365168a99b82d88bdcc580894e41853701dcdfe1
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2016-12-01 16:27:33 -0600

    new colors and improved elements

---
+++ js/init.js
@@ -0,0 +1,20 @@
+$(function() { 
+  
+  
+//  setTabContentHeight();
+  
+  
+});//end doc ready
+
+
+
+//$(window).resize(function() {
+//  setTabContentHeight();
+//});
+
+
+//function setTabContentHeight() {
+//  console.log($(window).height());
+//  console.log($('header').height());
+//  $('.tab-content').height($(window).height()-$('header').outerHeight()-$('.tab-header').outerHeight()-6);
+//}
