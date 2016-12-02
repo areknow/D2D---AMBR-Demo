64c5fb6382b5dac4bf3f0a38c55594c878c3baa2
Crowther, Arnaud <arnaud.crowther@dynatrace.com>
2016-12-02 13:44:39 -0600

    data table integration and customization callbacks

--- js/init.js
+++ js/init.js
@@ -1,20 +1,75 @@
 $(function() { 
   
   
-//  setTabContentHeight();
+  
+  $('#toggle-inputs').click(function() {
+    $('.input-row').slideToggle();
+  });
   
   
-});//end doc ready
+  
+  var table = $('#buyers').DataTable({
+      bFilter: false,
+      "dom": '<"wrapper"ilpt>',
+      "oLanguage": {
+        "sInfo": "Showing _START_ of _TOTAL_",
+        "sLengthMenu": "Show _MENU_",
+        "oPaginate": {
+          "sPrevious": "Prev",
+          "sNext": "Next"
+        }
+      },
+      "fnInfoCallback": function( oSettings, iStart, iEnd, iMax, iTotal, sPre ) {
+        perPage = iEnd - iStart + 1;
+        totalRatio = iTotal/perPage;
+        intTotalRatio = parseInt(totalRatio, 10);
+        totalPages = totalRatio > intTotalRatio ? intTotalRatio + 1 : intTotalRatio;
+        currentRatio = iStart/perPage;
+        intCurrentRatio = parseInt(currentRatio, 10);
+        currentPage = currentRatio > intCurrentRatio ? intCurrentRatio + 1 : intCurrentRatio;
+        $('#page-count').html('Page ' + currentPage + ' of ' + totalPages);
+        $('#page-count').insertAfter('#buyers_paginate');
+        return 'Showing ' + currentPage + ' of ' + iMax;
+      }
+  });
+  
+  
+  
+  
+  
+  
+  
+  
 
 
+  
+  
+  
+  
+  
+  
+  
+  
 
-//$(window).resize(function() {
-//  setTabContentHeight();
-//});
+  
+  $('#addRow').click(function() {
+    var company = $('#in-company').val();
+    var industry = $('#in-industry').val();
+    var date = $('#in-date').val();
+    table.row.add([
+      company,
+      industry,
+      date
+    ]).draw(true);
+    $('#page-count').insertAfter('#buyers_paginate');
+  });
+  
+  
+  
+  
+  
+  
+  
+  
+});
 
-
-//function setTabContentHeight() {
-//  console.log($(window).height());
-//  console.log($('header').height());
-//  $('.tab-content').height($(window).height()-$('header').outerHeight()-$('.tab-header').outerHeight()-6);
-//}
