a3cc02146cf32fcdc4ca374a022dc78a9e16bcf2
Crowther, Arnaud <arnaud.crowther@dynatrace.com>
2016-12-07 16:25:39 -0600

    fighting against the sandbox system

--- assets/js/console-message.js
+++ assets/js/console-message.js
@@ -5,5 +5,5 @@ console.log("%cHopefully you like what you see and we can talk some more ツ", "
 console.log("");
 console.log("%cI am extremely passionate about building awesome web things for people to interact with. I think the web is a valuable medium and should be crafted with precision and care. I'm currently looking for a great team to call my family and an exciting company to call home.", "color: grey; font-size: medium"); 
 console.log("");
-console.log("%cYou can creep on more of my work from my personal site (arnaud.cr), my portfolio site (miniml.co), or my github (@areknow). I also mess around with my camera (@areknowsee) when im not busy filling brackets.", "color: grey; font-size: medium");
+console.log("%cYou can creep on more of my work from my personal site (arnaud.cr), my portfolio site (miniml.co), or my github (@areknow). I also mess around with my camera (@areknowsee) when I’m not busy filling brackets.", "color: grey; font-size: medium");
 console.log("");
