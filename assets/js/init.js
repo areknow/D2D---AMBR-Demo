eda4b3839141dcf47bce8ca79d6668e56faf3356
Crowther, Arnaud <arnaud.crowther@dynatrace.com>
2016-12-15 14:26:51 -0600

    replaced all jQuery with javascript, removed all other dependencies

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,113 +1,58 @@
-$(function() { 
-  
-  
-  // add buyer button slide down click
-  $('#toggle-inputs').click(function() {
-    $('.input-row').slideToggle(100);
-    $('.green-button .fa').toggleClass("fa-minus-circle").toggleClass("fa-plus-circle");
-  });
-  
-  
-  // input label float functions
-  $("input").blur(function() {
-    $(this).parent().toggleClass("input-active");
-    if($(this).val()) {$(this).parent().toggleClass('input-active');}
-  });
-  $("input").focus(function() {
-    $(this).parent().toggleClass("input-active");
-    if($(this).val()) {$(this).parent().toggleClass('input-active');}
-  });
-  
-  
-  // input save button disable/enable
-  $('input').bind('input', function() {
-    if ($("#in-company").val()!="" && 
-        $("#in-industry").val()!="" && 
-        $("#in-date").val()!="") {
-      $("#addRow").removeClass("disabled");
-    } else {
-      $("#addRow").addClass("disabled");
-    }
-  });
-  
-  
-  // init buyers data table with custom options
-  var table = $('#buyers').DataTable({
-    bFilter: false,
-    "lengthMenu": [10,20],
-    "dom": '<"wrapper"ilpt>',
-    "oLanguage": {
-      "sInfo": "Showing _START_ of _TOTAL_",
-      "sLengthMenu": "Show _MENU_",
-      "oPaginate": {
-        "sPrevious": "Prev",
-        "sNext": "Next"
-      }
-    },
-    "fnInfoCallback": function( oSettings, iStart, iEnd, iMax, iTotal, sPre ) {
-      perPage = iEnd - iStart + 1;
-      totalRatio = iTotal/perPage;
-      intTotalRatio = parseInt(totalRatio, 10);
-      totalPages = totalRatio > intTotalRatio ? intTotalRatio + 1 : intTotalRatio;
-      currentRatio = iStart/perPage;
-      intCurrentRatio = parseInt(currentRatio, 10);
-      currentPage = currentRatio > intCurrentRatio ? intCurrentRatio + 1 : intCurrentRatio;
-      $('#page-count').html('Page ' + currentPage + ' of ' + totalPages);
-      $('#page-count').insertBefore('#buyers_paginate');
-      return 'Showing ' + perPage + ' of ' + iMax;
-    }
-  });
-  
-  
-  // add buyer table row functionality
-  $('#addRow').click(function() {
-    var company = $('#in-company');
-    var industry = $('#in-industry');
-    var date = $('#in-date');
-    table.row.add([
-      company.val(),
-      industry.val(),
-      date.val()
-    ]).draw(true);
-    $('#page-count').insertAfter('#buyers_paginate');
-    company.val("");
-    industry.val("");
-    date.val("");
-    $("#addRow").addClass("disabled");
-    $("input").parent().toggleClass("input-active");
-  });
-  
-  
-  // mobile menu click button
-  $('.mobile-menu').click(function() {
-    if ($('.tabs').hasClass('mobile-menu-open')) {
-      $('.tab-label').css('z-index','0');
-      $('.tabs').toggleClass('mobile-menu-open');
-    } else {
-      $('.tab-label').css('z-index','2');
-      $('.tabs').toggleClass('mobile-menu-open');
-    }
-  });
-  
-  
-  // mobile menu item click
-  $('.tab-label').click(function() {
-    $('.mobile-menu').click();
-  });
-  
+getRowCount();
 
+//open and close the modal
+var dialog = document.getElementById("addBuyerDialog"); 
+function showDialog() { 
+  dialog.style.display="block";
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-});// end doc ready
+} 
+function closeDialog() { 
+  dialog.style.display="none";
+}
 
+//insert a new row to the table
+function insertRow() {
+  var table = document.getElementById("buyers");
+  var company = document.getElementById("in-company").value; 
+  var industry = document.getElementById("in-industry").value; 
+  var date = document.getElementById("in-date").value; 
+  if (company!=="" && industry!=="" && date!=="") {
+    var row = table.insertRow(1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = company;
+    cell2.innerHTML = industry;
+    cell3.innerHTML = date;
+    document.getElementById("in-company").value = "";
+    document.getElementById("in-industry").value = "";
+    document.getElementById("in-date").value = "";
+    closeDialog();
+    getRowCount();
+  }
+}
 
+//add the row count to the drop down
+function getRowCount() {
+  var rows = document.getElementById("buyers").rows.length;
+  document.getElementById("numOfRows").innerHTML = rows;
+}
+
+//open and close mobile menu
+function openMenu() {
+  var tabMain = document.getElementById("tab-main");
+  if (tabMain.classList.contains('menu-open')) {
+    var el = document.getElementsByClassName("tab-label");
+    for (var i = 0, ilen = el.length - 1; i < ilen; i++) {
+      el[i].style.zIndex="0";
+    }
+    tabMain.classList.toggle('menu-open');
+  } else {
+    var el = document.getElementsByClassName("tab-label");
+    for (var i = 0, ilen = el.length - 1; i < ilen; i++) {
+      el[i].style.zIndex="10";
+    }
+    tabMain.classList.toggle('menu-open');
+  }
+}
 
